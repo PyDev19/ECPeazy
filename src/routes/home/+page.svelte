ee44f3ecbd75accd0ee8f3406f71637acc169d77
PyDev19 <atharvamishra956+1@gmail.com>
2024-06-26 06:28:04 -0500

    added home page

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { firebase_auth } from "$lib/firebase/firebase_app";
+
+    const current_user = firebase_auth.currentUser;
+</script>
+
+<section class="h-full flex flex-col justify-center items-center font-sans">
+    <div class="opacity-bg p-5 rounded-lg text-center sm:w-[90%] md:w-[70%]">
+        <h2 class="text-[3em] m-0">Welcome <span class="bg-gradient-to-b from-green-600 via-green-400 to-green-300 text-transparent bg-clip-text">{current_user?.displayName}</span></h2>
+    </div>
+</section>
