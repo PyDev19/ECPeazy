1ca30c6408f2280f120c35f51f04ec98021139c9
PyDev19 <atharvamishra956+1@gmail.com>
2024-06-26 06:28:33 -0500

    added signup through google

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
     import { type SignUpUser } from "$lib/types/user";
     import { sign_up } from "$lib/firebase/sign_up";
+    import BusyIndicator from "$lib/components/BusyIndicator.svelte";
+    import { is_auth_error } from "$lib/type_checks";
+    import { GoogleAuthProvider, signInWithCredential } from "firebase/auth";
+    import { firebase_auth } from "$lib/firebase/firebase_app";
+    import { type CredentialResponse } from "$lib/types/credentials";
+    import { onMount } from "svelte";
 
     let user: SignUpUser = {
         first_name: "",
@@ -12,7 +18,7 @@
         username: "",
         email: "",
         password: "",
-        confirm_password: ""
+        confirm_password: "",
     };
 
     let first_name_input: HTMLInputElement;
@@ -28,16 +34,24 @@
 
     let step = 0;
     let show_error = false;
+    let show_indicator = false;
 
     let message = "";
+    let modal_message = "Signing up...";
 
     function next() {
         if (step < 1) {
-            const inputs = [first_name_input, last_name_input, school_input, age_input, graduation_year_input];
+            const inputs = [
+                first_name_input,
+                last_name_input,
+                school_input,
+                age_input,
+                graduation_year_input,
+            ];
             let has_error = false;
             show_error = false;
 
-            inputs.forEach(input => {
+            inputs.forEach((input) => {
                 if (!input.value) {
                     input.style.border = "1px solid red";
                     has_error = true;
@@ -62,11 +76,17 @@
     }
 
     function submit() {
-        const inputs = [username_input, email_input, password_input, confirm_password_input];
+        show_indicator = true;
+        const inputs = [
+            username_input,
+            email_input,
+            password_input,
+            confirm_password_input,
+        ];
         let has_error = false;
         show_error = false;
 
-        inputs.forEach(input => {
+        inputs.forEach((input) => {
             if (!input.value) {
                 input.style.border = "1px solid red";
                 has_error = true;
@@ -81,9 +101,11 @@
         }
 
         // Password validation
-        const password_regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+        const password_regex =
+            /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
         if (!password_regex.test(user.password)) {
-            message = "Password must contain at least 8 characters, including uppercase, lowercase, and numbers."
+            message =
+                "Password must contain at least 8 characters, including uppercase, lowercase, and numbers.";
             show_error = true;
             return;
         }
@@ -104,16 +126,53 @@
         }
 
         // Submit form
-        sign_up(user).then(result => {
-            if (result) {
-                
-                console.log("User signed up successfully.");
+        sign_up(user).then((result) => {
+            show_indicator = false;
+            if (is_auth_error(result)) {
+                show_error = true;
+                message = result.message;
             } else {
-                console.log("User sign up failed.");
+                firebase_auth.signOut();
+                window.location.href = "/login";
             }
-        })
+        });
     }
+
+    function handle_google_auth(response: CredentialResponse) {
+        const credential = GoogleAuthProvider.credential(response.credential);
+
+        signInWithCredential(firebase_auth, credential).then((result) => {
+            firebase_auth.signOut();
+            window.location.href = "/login";
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+
+    onMount(() => {
+        (<any>window).google.accounts.id.initialize({
+            client_id: "162926619471-rdurosn21b96ur002q4vqqmd8uuauqvd",
+            callback: handle_google_auth,
+        });
+        (<any>window).google.accounts.id.renderButton(
+            document.getElementById("google_button"),
+            {
+                theme: "outline",
+                size: "large",
+                text: "sign_up",
+                shape: "pill",
+                logo_alignment: "left",
+                width: 200,
+                height: 100,
+                longtitle: true,
+                use_fedcm_for_prompt: false,
+            }
+        );
+        (<any>window).google.accounts.id.prompt();
+    });
 </script>
+
+<BusyIndicator show={show_indicator} message={modal_message} />
 
 <section class="h-full flex flex-col justify-center items-center font-sans">
     <div class="opacity-bg p-5 rounded-lg text-center sm:w-[90%] md:w-[70%]">
@@ -132,17 +191,20 @@
                                 bind:this={first_name_input}
                                 placeholder="First Name"
                                 class="p-2 rounded-lg signup-input flex-1"
+                                name="first_name"
                             />
                             <input
                                 bind:value={user.middle_name}
                                 placeholder="Middle Name (Optional)"
                                 class="p-2 rounded-lg signup-input flex-1"
+                                name="middle_name"
                             />
                             <input
                                 bind:value={user.last_name}
                                 bind:this={last_name_input}
                                 placeholder="Last Name"
                                 class="p-2 rounded-lg signup-input flex-1"
+                                name="last_name"
                             />
                         </div>
                         <input
@@ -171,12 +233,26 @@
                             name="graduation_year"
                         />
                         {#if show_error}
-                            <p class="text-red-500">Please fill out all the required fields.</p>
+                            <p class="text-red-500">
+                                Please fill out all the required fields.
+                            </p>
                         {/if}
-                        <button
-                            type="button"
-                            class="p-2 rounded-lg bg-blue-500 text-white"
-                            on:click={next}>Next</button>
+                        <div class="flex flex-col space-y-2">
+                            <button
+                                type="button"
+                                class="p-2 rounded-lg bg-blue-500 text-white max-w-[200px] mx-auto"
+                                on:click={next}>Next</button
+                            >
+                            <div id="google_button" class="mx-auto"></div>
+                            <p class="text-gray-500 font-bold">
+                                Already have an account?
+                            </p>
+                            <a
+                                href="/login"
+                                class="p-2 rounded-lg bg-blue-500 text-white max-w-[200px] mx-auto"
+                                >Login</a
+                            >
+                        </div>
                     </form>
                 </div>
                 <div class="form-slide">
@@ -186,9 +262,11 @@
                             bind:this={username_input}
                             placeholder="Username"
                             class="p-2 rounded-lg signup-input"
+                            autocomplete="username"
                         />
                         <input
                             type="email"
+                            name="email"
                             autocomplete="email"
                             bind:value={user.email}
                             bind:this={email_input}
@@ -198,6 +276,7 @@
                         />
                         <input
                             type="password"
+                            name="password"
                             autocomplete="new-password"
                             bind:value={user.password}
                             bind:this={password_input}
@@ -206,6 +285,7 @@
                         />
                         <input
                             type="password"
+                            name="confirm_password"
                             autocomplete="new-password"
                             bind:value={user.confirm_password}
                             bind:this={confirm_password_input}
