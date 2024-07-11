8ca996dd67bd4535ead35a93b120765c67f65241
PyDev19 <atharvamishra956+1@gmail.com>
2024-07-10 22:19:48 -0500

    chore: Update login function to return User object instead of boolean

--- src/lib/firebase/login.ts
+++ src/lib/firebase/login.ts
@@ -1,9 +1,9 @@
 import { firebase_auth } from "./firebase_app";
-import { signInWithEmailAndPassword, type AuthError } from "firebase/auth";
+import { signInWithEmailAndPassword, type AuthError, type User } from "firebase/auth";
 
-export async function login(email: string, password: string): Promise<boolean | AuthError> {
-    return signInWithEmailAndPassword(firebase_auth, email, password).then(() => {
-        return true;
+export async function login(email: string, password: string): Promise<User | AuthError> {
+    return signInWithEmailAndPassword(firebase_auth, email, password).then((user) => {
+        return user.user;
     }).catch((error: AuthError) => {
         console.log(error.code, error.message)
         return error;
