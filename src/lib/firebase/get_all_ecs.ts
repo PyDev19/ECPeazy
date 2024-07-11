1fcd11e87fbdc6ec5cce8aa366f4c3390edab4ec
PyDev19 <atharvamishra956+1@gmail.com>
2024-07-10 19:36:13 -0500

    removed comments

--- src/lib/firebase/get_all_ecs.ts
+++ src/lib/firebase/get_all_ecs.ts
@@ -2,21 +2,17 @@ import type { EC } from "$lib/types/database";
 import { FirestoreError, collection, getDocs } from "firebase/firestore";
 import { firebase_firestore } from "./firebase_app";
 
-export async function get_all_ecs(): Promise<EC[] | FirestoreError> {
-    // Get the collection of ECs
+export async function get_all_ecs() {
     const ecs_collection = collection(firebase_firestore, "ECs");
 
-    // Retrieve all the documents in the collection
-    return getDocs(ecs_collection).then(docs => {
+    return getDocs(ecs_collection).then(async docs => {
         const ecs: EC[] = [];
 
-        // Iterate through each document
         docs.forEach((doc) => {
             const doc_data: EC = doc.data() as EC;
             ecs.push(doc_data);
         });
 
-        // Return the ECs
         return ecs;
     }).catch((err: FirestoreError) => {
         return err;
