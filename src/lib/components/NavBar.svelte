3f192d9e0fd370a9cec261ceba13a1759f810677
PyDev19 <atharvamishra956+1@gmail.com>
2024-09-01 23:28:48 -0500

    added UID check for new portfolio path

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
     import { onMount } from "svelte";
-    import { firebase_auth } from "$lib/firebase/firebase_app";
+    import { firebase_auth } from "$lib/firebase/firebase.app";
     import type { User } from "firebase/auth";
     import { onAuthStateChanged } from "firebase/auth";
 
     let show_profile = false;
     let profile_image: string | null = "";
     let show_menu = false;
+    let uid = "";
 
     function logout() {
         firebase_auth.signOut();
@@ -17,6 +18,7 @@
     onMount(() => {
         onAuthStateChanged(firebase_auth, (user: User | null) => {
             if (user) {
+                uid = user.uid;
                 firebase_auth.updateCurrentUser(user);
                 console.log("User, " + user.displayName + " is logged in");
 
@@ -29,33 +31,46 @@
     });
 </script>
 
-<div class="flex flex-row justify-between z-50 absolute w-full">
+<div class="flex flex-row justify-between z-30 absolute w-full">
     <div class="bg-[#FFE8A3] inline-flex flex-row space-x-20 mt-3 ml-3 p-1 rounded-lg">
         <div class="flex flex-row space-x-2 my-auto">
-            <img src="favicon_transparent.png" alt="ECPeazy Logo" width="50" height="50" />
+            <img src="/favicon_transparent.png" alt="ECPeazy Logo" width="50" height="50" />
             <h1 class="my-auto text-[#52554C] font-bold text-2xl">
                 <span class="text-[#7B9150]">EC</span>Peazy
             </h1>
         </div>
         <div class="flex flex-row space-x-5 my-auto text-lg font-bold text-[#627368]">
             <a href="/search">Find ECs</a>
-            <a href="/portfolio" class="pr-3">My Portfolio</a>
+            {#if uid !== ""}
+                <a href="/portfolio/{uid}" class="pr-3">My Portfolio</a>
+            {:else}
+                <a href="/login" class="pr-3">My Portfolio</a>
+            {/if}
         </div>
     </div>
     {#if show_profile}
         <div class="inline-flex rounded-full bg-[#FFE8A3] my-auto p-2 mr-5 w-12">
-            <button on:click={() => {
-                show_menu = !show_menu;
-            }}>
+            <button
+                on:click={() => {
+                    show_menu = !show_menu;
+                }}
+            >
                 <img src={profile_image} alt="Profile Pic" class="rounded-full" width="40" />
             </button>
         </div>
         {#if show_menu}
             <div class="flex flex-col absolute top-16 right-14 bg-white rounded-lg">
-                <a href="/portfolio" class="flex flex-row space-x-2 p-4 hover:bg-gray-200 rounded-t-lg">
-                    <img src="portfolio.svg" alt="portfolio" class="my-auto" />
-                    <p class="my-auto">Porfolio</p>
-                </a>
+                {#if uid === ""}
+                    <a href="/login" class="flex flex-row space-x-2 p-4 hover:bg-gray-200 rounded-t-lg">
+                        <img src="portfolio.svg" alt="portfolio" class="my-auto" />
+                        <p class="my-auto">Porfolio</p>
+                    </a>
+                {:else}
+                    <a href="/portfolio/{uid}" class="flex flex-row space-x-2 p-4 hover:bg-gray-200 rounded-t-lg">
+                        <img src="portfolio.svg" alt="portfolio" class="my-auto" />
+                        <p class="my-auto">Porfolio</p>
+                    </a>
+                {/if}
                 <button on:click={logout} class="flex flex-row space-x-2 p-4 hover:bg-gray-200 rounded-b-lg">
                     <img src="logout.svg" alt="logout" class="my-auto" />
                     <p class="my-auto">Logout</p>
