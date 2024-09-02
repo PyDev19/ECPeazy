2291c334d732195ca8f4762533b03f06b5fa5bff
PyDev19 <atharvamishra956+1@gmail.com>
2024-09-01 19:57:55 -0500

    removed function for google signups and added major 1 and 2 as fields in documents and changed age to grade

--- src/lib/firebase/sign_up.ts
+++ src/lib/firebase/sign_up.ts
@@ -19,15 +19,16 @@ export async function sign_up(user: SignUpUser): Promise<User | AuthError> {
 
         const user_doc_ref = doc(firebase_firestore, 'Users', current_user.uid);
         await setDoc(user_doc_ref, {
-            sign_up_complete: false,
             username: user.username,
             email: user.email,
             first_name: user.first_name,
             middle_name: user.middle_name,
             last_name: user.last_name,
             school: user.school,
-            age: user.age,
-            graduation_year: user.graduation_year
+            grade: user.grade,
+            graduation_year: user.graduation_year,
+            major_1: user.major_1,
+            major_2: user.major_2,
         });
 
         return user_credential.user;
@@ -36,45 +37,4 @@ export async function sign_up(user: SignUpUser): Promise<User | AuthError> {
 
         return error;
     });
-}
-
-export async function google_sign_up(response: CredentialResponse) {
-    const credential = GoogleAuthProvider.credential(response.credential);
-
-    signInWithCredential(firebase_auth, credential).then((result) => {
-        const user = result.user;
-
-        const user_doc_ref = doc(firebase_firestore, 'Users', user.uid);
-        setDoc(user_doc_ref, {
-            email: user.email,
-        });
-
-        window.location.href = "/signup/google_complete";
-    }).catch((error) => {
-        console.log(error);
-    });
-}
-
-export async function google_sign_up_complete(user: GoogleUser) {
-    const current_user = firebase_auth.currentUser;
-
-    updateProfile(current_user!, {
-        displayName: user.username
-    }).then(() => {
-        console.log('User profile updated successfully');
-    }).catch((error) => { console.log(error); });
-
-    const user_doc_ref = doc(firebase_firestore, 'Users', current_user!.uid);
-    await setDoc(user_doc_ref, {
-        username: user.username,
-        first_name: user.first_name,
-        middle_name: user.middle_name,
-        last_name: user.last_name,
-        school: user.school,
-        age: user.age,
-        graduation_year: user.graduation_year,
-    });
-
-    firebase_auth.signOut()
-    window.location.href = "/login";
 }
