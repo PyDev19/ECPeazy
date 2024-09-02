79a48c04c84748412fd9888f65e0af5c1c4f9db8
PyDev19 <atharvamishra956+1@gmail.com>
2024-09-01 23:27:35 -0500

    renamed firebase app file

---
+++ src/lib/firebase/firebase.app.ts
@@ -0,0 +1,40 @@
+import { initializeApp, getApps, type FirebaseApp } from 'firebase/app';
+import { initializeAuth, type Auth } from 'firebase/auth';
+import { getFirestore, type Firestore } from 'firebase/firestore';
+import { browserLocalPersistence, browserSessionPersistence } from 'firebase/auth';
+import {
+    PUBLIC_FIREBASE_API_KEY,
+    PUBLIC_FIREBASE_AUTH_DOMAIN,
+    PUBLIC_FIREBASE_PROJECT_ID,
+    PUBLIC_FIREBASE_STORAGE_BUCKET,
+    PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    PUBLIC_FIREBASE_APP_ID,
+    PUBLIC_FIREBASE_MEASUREMENT_ID
+} from '$env/static/public';
+
+const config = {
+    apiKey: PUBLIC_FIREBASE_API_KEY,
+    authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: PUBLIC_FIREBASE_APP_ID,
+    measurementId: PUBLIC_FIREBASE_MEASUREMENT_ID
+};
+
+let firebase_app: FirebaseApp | undefined;
+
+if (!getApps().length) {
+    firebase_app = initializeApp(config);
+}
+else {
+    firebase_app = getApps()[0]
+}
+
+let firebase_auth: Auth = initializeAuth(firebase_app, {
+    persistence: [browserLocalPersistence, browserSessionPersistence],
+});
+firebase_auth.useDeviceLanguage();
+let firebase_firestore: Firestore = getFirestore(firebase_app);
+
+export { firebase_app, firebase_auth, firebase_firestore }
